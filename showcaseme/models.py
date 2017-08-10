c336b6f4f24639807f319e37c676a36fabecb600
Antigluon <optionaloctave@gmail.com>
2017-08-10 18:45:12 -0400

    Added a fucntion to search for users by relevance. To filter, use userSearch(requirements, threshold=1)

--- showcaseme/models.py
+++ showcaseme/models.py
@@ -13,3 +13,27 @@ class User(UserMixin):
 		self.id = uid
 		self.userType = user_type
 		self.name = getUserData(uid)['name']
+
+#returns a dictionary {userID:match} where match is a decimal betweeon 0.0 and 1.0, where 1.0 is a perfect match and 0.0 is a total miss. Only returns users that are above a certain threshold
+def userSearch(requirements, bonusReqs, requirementWeight=1.0, bonusWeight=0.5, threshold=0.3):
+	foundUsers = {} 
+	for user in users.all():
+		points = 0.0
+		userTags = {tag['name']:tag['skill'] for tag in user.tags}
+		requirements = {tag['name']:tag['skill'] for tag in requirements}
+		bonusReqs = {tag['name']:tag['skill'] for tag in bonusReqs}
+		maxPoints = requirementWeight * sum([1+val for val in requirements.values()]) + bonusWeight * sum([1+val for val in bonusReqs.values()])
+		for tag in userTags.keys()
+			if tag in requirements:
+				if userTags[tag] > requirements[tag]:
+					points += requirementWeight * (1+requirements[tag])
+				else: #User skill <= required skill
+					points += requirementWeight * (1+userTags[tag])
+			elif tag in bonusReqs:
+				if userTags[tag] > bonusReqs[tag]:
+					points += bonusWeight * (1+bonusReqs[tag])
+				else: #User skill <= required skill
+					points += bonusWeight * (1+userTags[tag])
+		if points/maxPoints > threshold:
+			foundUsers[user.id] = points/maxPoints
+	return foundUsers
