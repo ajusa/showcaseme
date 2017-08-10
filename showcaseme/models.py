4b1a08b986000948a0083a51dedf052023a40977
Antigluon <optionaloctave@gmail.com>
2017-08-10 19:26:10 -0400

    In progress earch function -- throwing utterly nonsense errors

--- showcaseme/models.py
+++ showcaseme/models.py
@@ -15,15 +15,15 @@ class User(UserMixin):
 		self.name = getUserData(uid)['name']
 
 #returns a dictionary {userID:match} where match is a decimal betweeon 0.0 and 1.0, where 1.0 is a perfect match and 0.0 is a total miss. Only returns users that are above a certain threshold
-def userSearch(requirements, bonusReqs, requirementWeight=1.0, bonusWeight=0.5, threshold=0.3):
+def userSearch(requirements, bonusReqs=[], requirementWeight=1.0, bonusWeight=0.5, threshold=0.3):
 	foundUsers = {} 
 	for user in users.all():
 		points = 0.0
-		userTags = {tag['name']:tag['skill'] for tag in user.tags}
-		requirements = {tag['name']:tag['skill'] for tag in requirements}
-		bonusReqs = {tag['name']:tag['skill'] for tag in bonusReqs}
+		userTags = {tag['name']: tag['skill'] for tag in user['profile']['tags']}
+		requirements = {tag['name']: tag['skill'] for tag in requirements}
+		bonusReqs = {tag['name']: tag['skill'] for tag in bonusReqs}
 		maxPoints = requirementWeight * sum([1+val for val in requirements.values()]) + bonusWeight * sum([1+val for val in bonusReqs.values()])
-		for tag in userTags.keys()
+		for tag in userTags.keys():
 			if tag in requirements:
 				if userTags[tag] > requirements[tag]:
 					points += requirementWeight * (1+requirements[tag])
