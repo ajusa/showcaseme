1121eaf37ee3a4ce548a2f5fe891cd91c11d1dbb
Antigluon <optionaloctave@gmail.com>
2017-08-11 19:13:02 -0400

    fix

--- showcaseme/models.py
+++ showcaseme/models.py
@@ -12,7 +12,10 @@ def getUserData(id):
 class User(UserMixin):
 	def __init__(self, uid, user_type=''):
 		self.id = uid
-		self.userType = getUserData(uid)['userType']
+		if 'usertype' in getUserData(uid):
+			self.userType = getUserData(uid)['userType']
+		else:
+			userType = user_type
 		self.name = getUserData(uid)['name']
 
 #returns a dictionary {userID: matchPercent} where match is a decimal betweeon 0.0 and 1.0, where 1.0 is a perfect match and 0.0 is a total miss. Only returns users that are above a certain threshold
